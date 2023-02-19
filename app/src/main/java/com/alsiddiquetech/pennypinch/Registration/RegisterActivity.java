4a9ca7f93b9949b15d3a1592182286483e69ebc9
zia1122 <ziarehman.cs@gmail.com>
2023-02-19 18:26:50 +0500

    initial commit of my project

---
+++ app/src/main/java/com/alsiddiquetech/pennypinch/Registration/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.alsiddiquetech.pennypinch.Registration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.alsiddiquetech.pennypinch.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
